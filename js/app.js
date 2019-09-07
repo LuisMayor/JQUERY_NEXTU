74547b5a0ef1d3d0c134113c8dcb60ecc7d49d9f
Luis <luismayortsu@gmail.com>
2019-09-07 16:37:19 -0400

    funcion random

--- js/app.js
+++ js/app.js
@@ -38,6 +38,26 @@ function colorBlink(selector) {
 		});
 }
 
+//obtener los numeros aleatorios con Random
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function initGame() {
